d6b84e8fdc5a98e99e67cb19c3a6f03c301786d0
wjj <17752576852@139.com>
2026-08-01 14:21:20 +0800

    fix: remove redundant MyBatisPlusConfig from service modules, move pagination bean to main class

--- gym-parent-project/gym-service-user/src/main/java/com/v1/GymServiceUserApplication.java
+++ gym-parent-project/gym-service-user/src/main/java/com/v1/GymServiceUserApplication.java
@@ -1,9 +1,13 @@
 package com.v1;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDubbo
@@ -11,5 +15,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GymServiceUserApplication {
     public static void main(String[] args) {
         SpringApplication.run(GymServiceUserApplication.class, args);
+    }
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
     }
 }
--- gym-parent-project/gym-service-web/src/main/java/com/v1/GYMServiceWebApplication.java
+++ gym-parent-project/gym-service-web/src/main/java/com/v1/GYMServiceWebApplication.java
@@ -3,15 +3,9 @@ package com.v1;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.v1",
-    excludeFilters = @ComponentScan.Filter(
-        type = FilterType.REGEX,
-        pattern = "com\\.v1\\.service\\..*"))
 @EnableSwagger2
 @EnableDubbo
 public class GYMServiceWebApplication {
